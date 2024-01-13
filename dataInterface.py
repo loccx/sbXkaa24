15010338a308c2ac5b42b296ec18e5f583e0ef26
Akul Singh <75509984+AkulS1008@users.noreply.github.com>
2024-01-13 12:33:25 -0800

    Add files via upload
    Adding database-related files

---
+++ dataInterface.py
@@ -0,0 +1,69 @@
+from google.cloud import firestore
+
+# Replace 'your-project-id' with your actual Google Cloud project ID
+project_id = 'ecosia-411103'
+
+# Create a Firestore client
+db = firestore.Client(project=project_id)
+
+# Add data to Firestore
+def add_data():
+    data = {
+        'num_searches': 0
+    }
+    
+    # Add a document to the 'users' collection with a randomly generated ID
+    db.collection('users').add(data)
+    print('Data added successfully.')
+
+# Retrieve data from Firestore
+def get_data():
+    # Retrieve all documents from the 'users' collection
+    users_ref = db.collection('users')
+    docs = users_ref.stream()
+    fetchData = {}
+    for doc in docs:
+        print(f'{doc.id} => {doc.to_dict()}')
+        fetchData[doc.id] = doc.to_dict()
+    return fetchData
+
+# Update data in Firestore
+def update_data(document_id, updated_data):
+    doc_ref = db.collection('users').document(document_id)
+    doc_ref.update(updated_data)
+    print(f'Data updated successfully.')
+
+# Delete data from Firestore
+def delete_data(document_id):
+    doc_ref = db.collection('users').document(document_id)
+    doc_ref.delete()
+    print(f'Data deleted successfully.')
+
+if __name__ == '__main__':
+    data = get_data()
+    for id in data:
+        print(id, data[id])
+    
+    
+    # Add data
+    #add_data()
+
+    # Retrieve data
+    #get_data()
+
+    # Update data
+    """document_id_to_update = 'your_document_id_here'  # Replace with the actual document ID
+    updated_data = {'num_searches': 5}  # Replace with the updated data
+    update_data(document_id_to_update, updated_data)"""
+
+    # Retrieve updated data
+    get_data()
+
+    # Delete data
+    """document_ids = ['Fk7HZJltatbjVMR85lk7', '94JEEURGjhqPaCOtcB4s', 'O8v2nFevwPE13N6HJlDp']
+    for docId in document_ids:
+        document_id_to_delete = docId  # Replace with the actual document ID
+        delete_data(document_id_to_delete)"""
+
+    # Retrieve data after deletion
+    #get_data()
