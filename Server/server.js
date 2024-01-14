a9f5d3ae127d386a42bcbc1d1ed50316f27af472
Andrew Fong <122076326+AndrewByronFong@users.noreply.github.com>
2024-01-14 02:16:53 -0500

    Delete Server directory

--- Server/server.js
+++
@@ -1,8 +0,0 @@
-const http = require('http');
-const {app} = require("./Routing/app.js");
-
-const server = http.createServer(app);
-
-server.listen(3000, () => {
-  console.log(`Server is up on port 3000`);
-});
