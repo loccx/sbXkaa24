ab68f256f4b4f2a318e54b9ae947d7b3ea1a8814
Andrew Fong <Afong566@gmail.com>
2024-01-13 23:01:43 -0800

    finalized network and opanai integration

---
+++ Server/Routing/app.js
@@ -0,0 +1,98 @@
+const express = require("express");
+const cors = require("cors");
+const { searchEcosia } = require("./ecosia.controller");
+const { getMessages, sendMessage } = require("./openai.controller");
+const {
+  addData, // adds a new user
+  deleteAllInCollection, // deletes all users
+  incrementSearchHistory, // updates first user
+  getData, // gets first user
+} = require("../Model/datainterface.model");
+const morgan = require("morgan");
+
+const app = express();
+
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+
+app.use(morgan("dev"));
+
+app.use(express.json());
+
+app.post("/", (req, res) => {
+  searchEcosia(req.body.query)
+    .then(() => {
+      res.status(200).send("Success");
+    })
+    .catch((err) => {
+      res.status(500).send(err);
+    });
+});
+
+app.post("/tree", (req, res) => {
+  addData()
+    .then(() => {
+      res.send("Success");
+    })
+    .catch((err) => {
+      res.status(500).send(err);
+    });
+});
+
+app.get("/tree", (req, res) => {
+  getData()
+    .then((entry) => {
+      res.status(200).send(entry.data.num_searches.toString());
+    })
+    .catch((err) => {
+      res.status(500).send(err);
+    });
+});
+
+app.put("/tree", (req, res) => {
+  incrementSearchHistory()
+    .then(() => {
+      res.send("Success");
+    })
+    .catch((err) => {
+      res.status(500).send(err);
+    });
+});
+
+app.delete("/tree", (req, res) => {
+  deleteAllInCollection("users")
+    .then(() => {
+      res.send("Success");
+    })
+    .catch((err) => {
+      res.status(500).send(err);
+    });
+});
+
+app.get("/chat", (req, res) => {
+  getMessages()
+    .then((ans) => {
+      res.status(200).send(ans);
+    })
+    .catch((err) => {
+      res.status(500).send(err);
+    });
+});
+
+app.post("/chat", (req, res) => {
+  sendMessage(req.body.message)
+    .then(() => {
+      res.status(200).send("Success");
+    })
+    .catch((err) => {
+      console.log(req.body.message);
+      res.status(500).send(err);
+    });
+});
+
+module.exports = {
+  app,
+};
--- Server/server.js
+++ Server/server.js
@@ -1,29 +1,8 @@
-const express = require("express");
-const cors = require("cors");
-const { searchEcosia } = require("./ecosia.search");
+const http = require('http');
+const {app} = require("./Routing/app.js");
 
-const app = express();
+const server = http.createServer(app);
 
-app.use(
-  cors({
-    origin: "*",
-  })
-);
-
-app.post("/:query", (req, res) => {
-  searchEcosia(req.params.query)
-    .then(() => {
-      res.status(200).send("Success");
-    })
-    .catch((err) => {
-      res.status(500).send(err);
-    });
-});
-
-app.get("/treenumber", (req, res) => {
-  res.send("123456789");
-});
-
-app.listen(3000, () => {
-  console.log("Server running on port 3000");
+server.listen(3000, () => {
+  console.log(`Server is up on port 3000`);
 });
